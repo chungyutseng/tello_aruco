3e062d676e711ce90556d6f408a444b848396558
NTU <chungyutseng@example.com>
2021-11-11 20:44:20 +0800

    r

--- src/tello_aruco_test.py
+++ src/tello_aruco_test.py
@@ -8,6 +8,7 @@ import cv2.aruco as aruco
 import math
 from std_msgs.msg import Float32
 from sensor_msgs.msg import CompressedImage
+import time
 
 def convert_color_image(ros_image):
     bridge = CvBridge()
@@ -15,12 +16,12 @@ def convert_color_image(ros_image):
         color_image = bridge.imgmsg_to_cv2(ros_image, "bgr8")
         cv2.namedWindow("Color")
         cv2.imshow("Color", color_image)
-        cv2.waitKey(10)
+        k = cv2.waitKey(10)
         if k % 256 == 32:
             time_name = str(time.time())
             img_name = time_name + ".png"
             cv2.imwrite(img_name, color_image)
-            imag_counter = imag_counter + 1
+            # imag_counter = imag_counter + 1
     except CvBridgeError as e:
         print(e)
 
@@ -34,3 +35,7 @@ if __name__ == '__main__':
         tellodetect()
     except rospy.ROSInterruptException:
         pass
+
+
+# <node pkg="image_transport" name="image_compressed" type="republish" args="raw in:=image_raw compressed out:=image_raw" />
+# <node pkg="image_transport" name="image_compressed1" type="republish" args="h264 in:=image_raw raw out:=raw_image" />
